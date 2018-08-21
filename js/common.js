79b2faa9e9b15149267cf07847e004fc1a21c066
Dr.Dimas <dmitry.8060@gmail.com>
2018-08-21 18:17:30 +0300

    donson

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+$(".js-example-basic-hide-search").select2({
+    minimumResultsForSearch: Infinity,
+    width: "99.8%",
+    placeholder: "Ваш Город",
+    allowClear: true
+});});
